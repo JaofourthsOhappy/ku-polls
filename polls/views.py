d9a49ed8cf955bd9172f2be0e45ad9bfb454b1ec
Sittanat <apalakawongna@gmail.com>
2024-08-24 20:41:02 +0700

    part 6

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
-
 
 from .models import Choice, Question
 
@@ -21,7 +20,6 @@ class IndexView(generic.ListView):
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
@@ -31,7 +29,6 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
-
 
 class ResultsView(generic.DetailView):
     model = Question
