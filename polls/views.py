84c0d851822df877becf88ce12e23cc8232501a4
Sittanat <apalakawongna@gmail.com>
2024-09-04 23:52:12 +0700

    In class

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
-from .models import Choice, Question
+from .models import Choice, Question, Vote
 
 
 class IndexView(generic.ListView):
@@ -19,9 +20,7 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -39,19 +38,27 @@ class DetailView(generic.DetailView):
 
     def get(self, request, *args, **kwargs):
         """
-        Handel the Get request for the detail view.
+        Get the question object and render the template.
+
+        Args:
+            request: The HTTP request object.
+            self.object (int): The ID of the question being voted on.
+
+        Returns:
+            HttpResponseRedirect: Redirects to the index page with an error
+                                  message if the question is not published.
+            HttpResponse: Renders the results page.
         """
-        try:
-            question = get_object_or_404(Question, pk=kwargs["pk"])
-        except Http404:
-            messages.error(request,
-                           f"Poll number {kwargs['pk']} does not exists.")
-            return redirect("polls:index")
-        if not question.is_published():
-            messages.error(request,
-                           f"Poll number {question.id} Already closed.")
-            return redirect("polls:index")
-        return render(request, self.template_name, {"question": question})
+        # Get the question object
+        self.object = self.get_object()
+
+        # Check if voting is allowed
+        if not self.object.can_vote():
+            messages.error(request, "This poll is closed.")
+            return redirect('polls:index')
+
+        return self.render_to_response(self.get_context_data(
+                                        object=self.object))
 
 
 class ResultsView(generic.DetailView):
@@ -77,12 +84,13 @@ class ResultsView(generic.DetailView):
             return redirect("polls:index")
         return render(request, self.template_name, {"question": question})
 
-
+@login_required
 def vote(request, question_id):
     """
     Handles the user's vote for a poll question.
     """
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -91,11 +99,19 @@ def vote(request, question_id):
             'question': question,
             'error_message': "You didn't select a choice.",
         })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(
+    this_user = request.user
+    # else:
+    #     selected_choice.votes += 1
+    #     selected_choice.save()
+    try:
+        # find a vote for this user and this question
+        vote = Vote.objects.get(user=this_user, choice__question=question)
+        # update the vote after a user has changed their vote
+        vote.choice = selected_choice
+    except Vote.DoesNotExist:
+        # no matching vote - create a new vote object
+        vote = Vote.objects.create(user=this_user, choice=selected_choice)
+    vote.save()
+    messages.success(request, f"Your vote for '{selected_choice}' has been saved.")
+    return HttpResponseRedirect(
             reverse('polls:results', args=(question.id,)))
