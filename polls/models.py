c5f06d65679fee507debb28ba207f4cdc4b97a0f
Sittanat <apalakawongna@gmail.com>
2024-09-16 13:22:18 +0700

    fix:#38

--- polls/models.py
+++ polls/models.py
@@ -66,8 +66,8 @@ class Choice(models.Model):
     """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-    @property
     def votes(self):
         """
         Returns the number of votes for this choice.
